9e0e88e5bc36c2cf9812112fbf3e0418888bc161
collins-mwangi1 <collins.mwangi1@student.moringaschool.com>
2024-06-07 16:12:00 +0300

    lab completed

--- lib/testing/user_test.py
+++ lib/testing/user_test.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python3
-
 from user import User
 
 class TestUser:
-    '''Class "User" in user.py'''
-
     def test_is_class(self):
-        '''is a class.'''
-        assert(object in User.__bases__)
+        assert 'User' in str(User)
 
     def test_initializes_with_names(self):
-        '''initializes with first and last name.'''
         my_user = User("My", "User")
-        assert((my_user.first_name, my_user.last_name) == ("My", "User"))
+        assert (my_user.first_name, my_user.last_name) == ("My", "User")
